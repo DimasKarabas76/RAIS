79de7d361df8e3164bf3905e7f254ffcb88db854
Dmitry Chikin <123234416+DimasKarabas76@users.noreply.github.com>
2024-11-28 21:41:12 +0700

    Add files via upload

--- create.py
+++ create.py
@@ -1,25 +1,17 @@
 import os
-import datetime
 import subprocess
+from datetime import datetime
 
+def create_backup(db_name, user, password, backup_dir):
+    os.environ['PGPASSWORD'] = password
 
-def create_backup(db_name, db_user, db_password, backup_dir, host='localhost', port=5432):
-    if not os.path.exists(backup_dir):
-        os.makedirs(backup_dir)
-        print(f"Директория {backup_dir} не существует, она была создана.")
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    backup_file = os.path.join(backup_dir, f"{db_name}_backup_{timestamp}.sql")
 
-    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    backup_filename = f"{db_name}_backup_{timestamp}.sql"
-    backup_path = os.path.join(backup_dir, backup_filename)
+    command = f"pg_dump -U {user} -F c -b -v -f {backup_file} {db_name}"
+    subprocess.run(command, shell=True, check=True)
 
-    command = f"PGPASSWORD={db_password} pg_dump -U {db_user} -h {host} -p {port} {db_name} -f {backup_path}"
+    print(f"Backup of database {db_name} created at {backup_file}")
 
-    try:
-        subprocess.run(command, shell=True, check=True)
-        print(f"Резервная копия базы данных {db_name} успешно создана: {backup_path}")
-        return backup_path
-    except subprocess.CalledProcessError as e:
-        print(f"Ошибка при создании резервной копии: {e}")
-        return None
-
-#backup_file = create_backup('my_database', 'postgres', 'password', '/path/to/backup')
+if __name__ == "__main__":
+    create_backup('auth_db', 'postgres', 'dima777', '/pract_RAIS')
