11ca47b8488787145269d5c744c507b8d77856ad
Dmitry Chikin <123234416+DimasKarabas76@users.noreply.github.com>
2024-12-05 17:00:49 +0700

    Add files via upload

--- create.py
+++ create.py
@@ -20,7 +20,6 @@ def create_backup_ssh(db_name, user, remote_host, backup_dir, ssh_user, ssh_pass
 
         sftp = ssh.open_sftp()
         local_backup_path = os.path.join(backup_dir, f"{db_name}_backup_{timestamp}.dump")
-        sftp.get(local_backup_path)
         print(f"Backup file downloaded to: {local_backup_path}")
 
         # Закрываем соединения
