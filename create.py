8d6d67a6a054c7f51618d3fb6ded47759a7720f3
Dmitry Chikin <123234416+DimasKarabas76@users.noreply.github.com>
2024-12-05 17:12:17 +0700

    Add files via upload

--- create.py
+++ create.py
@@ -6,9 +6,10 @@ from datetime import datetime
 def create_backup_ssh(db_name, user, remote_host, backup_dir, ssh_user, ssh_password):
     # Формируем имя файла для резервной копии
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    backup_file = f"/var/lib/postgresql/dumps/{db_name}_backup_{timestamp}.dump"  # Создаем дамп в /tmp на удаленном сервере
 
     # Строим команду для выполнения через SSH
-    dump_command = f"pg_dump -U {user} -h localhost -F c -b -v {db_name} > {backup_dir}"
+    dump_command = f"pg_dump -U {user} -h localhost -F c -b -v {db_name} > {backup_file}"
 
     try:
         # Создаем SSH клиент
@@ -18,8 +19,22 @@ def create_backup_ssh(db_name, user, remote_host, backup_dir, ssh_user, ssh_pass
         # Подключаемся по паролю
         ssh.connect(remote_host, username=ssh_user, password=ssh_password)
 
+        # Выполняем команду на удаленной машине
+        stdin, stdout, stderr = ssh.exec_command(dump_command)
+
+        # Ожидаем завершения команды
+        stdout.channel.recv_exit_status()
+
+        # Печатаем вывод для отладки
+        print(stdout.read().decode())
+        error = stderr.read().decode()
+        if error:
+            print(f"Error: {error}")
+
+        # Скачиваем файл на локальный сервер, если нужно
         sftp = ssh.open_sftp()
         local_backup_path = os.path.join(backup_dir, f"{db_name}_backup_{timestamp}.dump")
+        sftp.get(backup_file, local_backup_path)
         print(f"Backup file downloaded to: {local_backup_path}")
 
         # Закрываем соединения
@@ -35,11 +50,13 @@ def run_periodically(interval, db_name, user, remote_host, backup_dir, ssh_user,
         time.sleep(interval)
 
 if __name__ == "__main__":
+    # Параметры для подключения и бэкапа
     db_name = 'auth_db'
     user = 'dima'
     remote_host = '172.25.204.9'
-    backup_dir = '/home/dima/pract_RAIS'
-    ssh_user = 'postgres'
-    ssh_password = '123'
+    backup_dir = '/home/dima/pract_RAIS'  # Локальная директория для сохранения дампов
+    ssh_user = 'postgres'  # Пользователь для SSH
+    ssh_password = '123'  # Пароль для SSH подключения
 
-    run_periodically(300, db_name, user, remote_host, backup_dir, ssh_user, ssh_password)
+    # Запуск бэкапов каждые 10 минут (600 секунд)
+    run_periodically(30, db_name, user, remote_host, backup_dir, ssh_user, ssh_password)
